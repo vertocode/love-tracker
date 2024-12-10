cb57e9d8bee45a9a028db4c06444f32a2d2ba040
vertocode <evertonvanoni1@gmail.com>
2024-12-10 10:11:17 -0300

    feat: add bg

--- app/app.vue
+++ app/app.vue
@@ -8,6 +8,6 @@
 
 <style lang="scss" scoped>
 .app {
-  background: url('https://img.freepik.com/vetores-gratis/fundo-de-dia-dos-namorados-com-lindos-coracoes-e-espaco-de-texto_1017-35938.jpg?t=st=1733835985~exp=1733839585~hmac=bc1891ff138e1568dc718d67ff9ff0cb43ae41ad2b1c431ee99b5358a3126f8e&w=2000');
+  background: url('~/assets/background.jpg') no-repeat center center ;
 }
 </style>
